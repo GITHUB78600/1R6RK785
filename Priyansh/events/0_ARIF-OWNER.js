80252b4005ce8a440b4f13fe28daf4ca9381b322
GITHUB78600 <githup3@yandex.com>
2024-09-21 16:45:20 +0530

    Update 0_ARIF-OWNER.js

--- Priyansh/events/0_ARIF-OWNER.js
+++ Priyansh/events/0_ARIF-OWNER.js
@@ -1,44 +1,57 @@
 module.exports.config = {
-	name: "god",
-	eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
-	version: "1.0.0",
-	credits: "team atf",
-	description: "Record bot activity notifications!",
+        name: "god",
+        eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
+        version: "1.0.0",
+        credits: "ARIF BABU",
+        description: "THIS BOT WAS MADE BY MR ARIF BABU",
     envConfig: {
         enable: true
     }
 };
 
-module.exports.run = async function({ api, event, Threads }) {
+module.exports.run = async function({ api, event, Threads, args, Users }) {
+
+         var datathread = await api.getThreadInfo(event.threadID);
+     var namethread = datathread.name;
+
+    const moment = require("moment-timezone");
+    var gio = moment.tz("Asia/Kolkata").format("HH:mm:ss D/MM/YYYY");
+
     const logger = require("../../utils/log");
     if (!global.configModule[this.config.name].enable) return;
-    var formReport =  "💸💸💸 Bot Notification 💸💸💸" +
-                        "\n\n💜 Thread mang ID: " + event.threadID +
-                        "\n😃 Action: {task}" +
-                        "\n😇 Action created by userID: " + event.author +
-                        "\n🥺 " + Date.now() +" 🙄",
-        task = "";
+    var formReport =  "❁ ═══════ ❃•❃ ═══════ ❁\n🔮            𝐀𝐑𝐈𝐅-𝐁𝐀𝐁𝐔             🔮\n❁ ═══════ ❃•❃ ═══════ ❁" +
+                        "\n\n━━━━━━━━━━━━━━━━━━━━━━\n🌺 𝖦ROUP NAME ❯ " + `${namethread}` +
+                        "\n🌺 GROUP UID ❯ " + event.threadID +
+                        "\n🌺 MESSAGES ❯ {task}" +
+
+                       "\n🌺 USER UID ❯ " + event.author +
+                        "\n" + Date.now() +
+                        "\n━━━━━━━━━━━━━━━━━━━━━━\n" +
+                        `DATE AND TIME ❯ ${gio}`,
+
+           task = "";
+
     switch (event.logMessageType) {
         case "log:thread-name": {
             const oldName = (await Threads.getData(event.threadID)).name || "Name does not exist",
-                    newName = event.logMessageData.name || "Name does not exist";
-            task = "User changes group name from: '" + oldName + "' to '" + newName + "'";
+                    newName = event.logMessageData.name || "Walang pangalan";
+            task = "THE USER CHANGED THE GROUP NAME '" + oldName + "' TO '" + newName + "'";
             await Threads.setData(event.threadID, {name: newName});
             break;
         }
         case "log:subscribe": {
-            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "The user added the bot to a new group!";
+            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "ADDED BY NEW GROUP";
             break;
         }
         case "log:unsubscribe": {
-            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "The user kicked the bot out of the group!"
+            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "BOT HAS BEEN KICKED OUT OF THE GROUP"
             break;
         }
         default: 
             break;
     }
 
-    if (task.length == 0) return;
+        if (task.length == 0) return;
 
     formReport = formReport
     .replace(/\{task}/g, task);
@@ -47,4 +60,4 @@ module.exports.run = async function({ api, event, Threads }) {
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
     });
-}
+      }
