6518c5385beb07d7b46aee29d9c714372589cda2
ARIFBABA786 <ab9046798@gmail.com>
2024-09-21 15:09:36 +0530

    Update 0_ARIF-FACEBOOK.js

--- Priyansh/commands/0_ARIF-FACEBOOK.js
+++ Priyansh/commands/0_ARIF-FACEBOOK.js
@@ -36,7 +36,7 @@ module.exports.handleEvent = async function({ api, event }) {
                 const attachment = fs.createReadStream(tempFilePath);
                 await api.sendMessage({
                     attachment,
-                    body: "Here's the video you requested:"
+                    body: "𝐘 𝐋𝐎 𝐕𝐈𝐃𝐄𝐎 ♥️😘"
                 }, event.threadID, (err) => {
                     if (err) console.error("Error sending message:", err);
                 });
